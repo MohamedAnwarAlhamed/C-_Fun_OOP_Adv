4c8fcc08786e19e6b33d6f0ab8c748ac3c6afce6
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2024-05-16 17:38:51 +0300

    Add files via upload

---
+++ Nested Types.cs
@@ -0,0 +1,56 @@
+/*
+- DEFINITION
+- WHY NESTED TYPES
+- PRIVATE/PUBLIC/INTERNAL
+- COMPOSITE OBJECTS
+- INITIALIZE NESTED OBJECT
+- CHECKLIST
+*/
+
+
+class A
+{
+    private int x;
+    class B
+    {
+        void Method()
+        {
+            A a = new A();
+            a.x = 10;
+        }
+    }
+}
+//===========================================================================
+//===========================================================================
+//===========================================================================
+Employee e1 = new Employee();
+Console.WriteLine(e1.EmployeeInsurance.CompanyName);
+Console.ReadKey();
+
+class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public Insurance EmployeeInsurance { get; set; }
+
+
+    public Employee() =>
+        EmployeeInsurance = new Insurance { PolicyId = -1, CompanyName = "N/A" };
+    public class Insurance
+    {
+        public int PolicyId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
+
+
+
+class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+
